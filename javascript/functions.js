bcbe0871b9df6bf113b2dbc3208efcb1954ecc4b
HARSHKR008 <i.harshk0008@gmail.com>
2025-03-10 21:38:20 +0530

    Update functions.js

---
+++ javascript/functions.js
@@ -0,0 +1,14 @@
+function addnums(a,b) {
+    c=a+b;
+    console.log(c);
+}
+ addnums(14,22);
+
+ //console.log(c);
+  const getavg= function(m1, m2, m3){
+    let avg=(m1+m2+m3)/3;
+    return avg;
+  }
+  const result = getavg(10,20,30)
+  console.log(result);
+  
