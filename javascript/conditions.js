c1c0f5c4bb6be9224820705f1e7251313876bf73
HARSHKR008 <i.harshk0008@gmail.com>
2025-03-10 12:17:39 +0530

    Update conditions.js

--- javascript/conditions.js
+++ javascript/conditions.js
@@ -1,5 +1,10 @@
 num1 = 22;
-if (condition) {
-    num1%2==0
-    console.log(even);
+if (num1%2==0) {
+    a= 'nice';
+    console.log('even');
+} else{
+    console.log('odd');
+    
 }
+console.log(a);
+
